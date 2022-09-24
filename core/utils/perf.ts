5beff4c8353e0379a6046328e196c0977b4d8b0e
Matthew Fan <kungfumo@gmail.com>
2022-09-24 13:01:26 -0400

    added perf

---
+++ core/utils/perf.ts
@@ -0,0 +1,21 @@
+const cache = new Map<string, number>();
+
+export const track = (name: string) => {
+  cache.set(name, performance.now());
+};
+
+export const logDuration = (name: string) => {
+  const start = cache.get(name);
+  if (!start) throw new Error(`"${name}" has not been tracked`);
+
+  const time = duration(cache.get(name)!, performance.now());
+  console.log(`Execution time of "${name}": ${format(time)}`);
+};
+
+const format = (num: number) => {
+  return num.toFixed(5);
+};
+
+const duration = (start: number, end: number) => {
+  return end - start;
+};
--- mod.ts
+++ mod.ts
@@ -1,10 +1,15 @@
 import { Site } from "./core/site.ts";
+import * as perf from "./core/utils/perf.ts";
 
 const start = () => {
+  perf.track("generate-site");
+
   const site = new Site({
     src: "./example",
   });
   site.generate();
+
+  perf.logDuration("generate-site");
 };
 
 start();
