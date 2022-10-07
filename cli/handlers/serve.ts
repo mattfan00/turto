d3398bd4dac5bc79e400608a10e6716b96129100
Matthew Fan <kungfumo@gmail.com>
2022-10-06 23:40:46 -0400

    deno fmt serve handler

--- cli/handlers/serve.ts
+++ cli/handlers/serve.ts
@@ -15,58 +15,58 @@ export const serveHandler = async (options: ServeOptions) => {
   console.log(`Started server on port ${options.port}\n`);
 
   await http.serveListener(listener, async (req) => {
-    const ctx: Context = { req, baseDir: site.getDest( )}
+    const ctx: Context = { req, baseDir: site.getDest() };
     return await logWrapper(ctx, serve);
   });
 };
 
 interface Context {
-  req: Request,
-  baseDir: string,
+  req: Request;
+  baseDir: string;
 }
 
 type Middleware = (ctx: Context, ...next: Middleware[]) => Promise<Response>;
 
 const serve: Middleware = async (ctx: Context) => {
-    if (ctx.req.method !== "GET") {
-      return new Response(null, { status: 405 });
-    }
+  if (ctx.req.method !== "GET") {
+    return new Response(null, { status: 405 });
+  }
 
-    const decodedUrl = decodeURI(ctx.req.url);
-    const url = new URL(decodedUrl);
+  const decodedUrl = decodeURI(ctx.req.url);
+  const url = new URL(decodedUrl);
 
-    const baseFilePath = path.join(ctx.baseDir, url.pathname);
-    const baseFile = getFileInfo(baseFilePath);
+  const baseFilePath = path.join(ctx.baseDir, url.pathname);
+  const baseFile = getFileInfo(baseFilePath);
 
-    if (!baseFile) {
-      return new Response("Not found", { status: 404 });
-    }
+  if (!baseFile) {
+    return new Response("Not found", { status: 404 });
+  }
 
-    if (baseFile.isFile) {
-      return await serveFile(ctx.req, baseFilePath, { fileInfo: baseFile });
-    }
+  if (baseFile.isFile) {
+    return await serveFile(ctx.req, baseFilePath, { fileInfo: baseFile });
+  }
 
-    // if directory then check if it contains an "index.html" file
-    const htmlFilePath =
-      (baseFilePath.endsWith("/") ? baseFilePath : baseFilePath + "/") +
-      "index.html";
-    const htmlFile = getFileInfo(htmlFilePath);
+  // if directory then check if it contains an "index.html" file
+  const htmlFilePath =
+    (baseFilePath.endsWith("/") ? baseFilePath : baseFilePath + "/") +
+    "index.html";
+  const htmlFile = getFileInfo(htmlFilePath);
 
-    // if no "index.html" file, then return 404
-    if (!htmlFile) {
-      return new Response("Not found", { status: 404 });
-    }
+  // if no "index.html" file, then return 404
+  if (!htmlFile) {
+    return new Response("Not found", { status: 404 });
+  }
 
-    // if directory doesn't end with "/" (http://example.com/test), redirect
-    if (!baseFilePath.endsWith("/")) {
-      return new Response(null, {
-        status: 301,
-        headers: { location: url + "/" },
-      });
-    } else {
-      return await serveFile(ctx.req, htmlFilePath, { fileInfo: htmlFile });
-    }
-}
+  // if directory doesn't end with "/" (http://example.com/test), redirect
+  if (!baseFilePath.endsWith("/")) {
+    return new Response(null, {
+      status: 301,
+      headers: { location: url + "/" },
+    });
+  } else {
+    return await serveFile(ctx.req, htmlFilePath, { fileInfo: htmlFile });
+  }
+};
 
 const logWrapper: Middleware = async (ctx: Context, next: Middleware) => {
   const res = await next(ctx);
