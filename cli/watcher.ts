4863ed5cfceed76a54ba1fe0068a8d31e66dc8f3
Matthew Fan <kungfumo@gmail.com>
2022-10-08 22:56:11 -0400

    simple middleware working

--- cli/watcher.ts
+++ cli/watcher.ts
@@ -1,29 +1,23 @@
 import { path } from "../deps.ts";
 
-export type Event = "create" | "modify" | "remove";
-
-export interface Options {
-  recursive: boolean;
-  base: string;
-}
-
-const defaultOptions: Options = {
-  recursive: true,
-  base: Deno.cwd(),
-};
+export type Middleware = (
+  ctx: Context,
+  next: () => Promise<void>,
+) => Promise<void> | void;
 
 export class Watcher {
   paths: string[] = [];
   options: Options;
+  #middlewares: Middleware[] = [];
 
   constructor(paths?: string | string[], options?: Partial<Options>) {
     this.options = { ...defaultOptions, ...options };
     if (paths) {
-      this.add(paths);
+      this.addPaths(paths);
     }
   }
 
-  add(paths: string | string[]) {
+  addPaths(paths: string | string[]) {
     const { base } = this.options;
     if (Array.isArray(paths)) {
       const modifiedPaths = paths.map((p) => path.join(base, p));
@@ -42,8 +36,50 @@ export class Watcher {
     });
 
     for await (const event of watcher) {
-      console.log(new Date());
-      console.log(event);
+      await this.#handleEvent(event);
     }
   }
+
+  async #handleEvent(event: Deno.FsEvent) {
+    const context: Context = {
+      path: event.paths,
+      file: Deno.statSync(event.paths[0]),
+      raw: event,
+    }
+
+    let currIndex = -1;
+
+    const next = async () => {
+      // TODO: add check for calling next() twice
+      currIndex += 1;
+      const middleware = this.#middlewares[currIndex];
+      if (!middleware) {
+        return
+      }
+
+      await middleware(context, next)
+    }
+
+    await next();
+  }
+
+  use(fn: Middleware) {
+    this.#middlewares.push(fn);
+  }
+}
+
+export interface Options {
+  recursive: boolean;
+  base: string;
+}
+
+const defaultOptions: Options = {
+  recursive: true,
+  base: Deno.cwd(),
+};
+
+export interface Context {
+  path: string[];
+  file: Deno.FileInfo;
+  raw: Deno.FsEvent;
 }
