908aa7db677ed404f8f4a00e5b907732fc668453
Matthew Fan <kungfumo@gmail.com>
2022-10-10 22:05:04 -0400

    use bishop for file watching

--- cli/handlers/build.ts
+++ cli/handlers/build.ts
@@ -1,6 +1,6 @@
 import { turto } from "../../mod.ts";
 import { styles } from "../utils.ts";
-import { Watcher } from "../watcher.ts";
+import { Watcher } from "../../deps.ts";
 
 export interface BuildOptions {
   src?: string;
@@ -13,10 +13,21 @@ export const buildHandler = async (options: BuildOptions) => {
   site.build();
 
   if (options.watch) {
-    const watcher = new Watcher();
+    const watcher = new Watcher(site.options.src);
     console.log(
-      `Started watching for changes in ${styles.file(site.getSrc())}`,
+      `Started watching for changes in ${styles.file(site.getSrc())}\n`,
     );
+
+    watcher.use((ctx) => {
+      const { event } = ctx;
+      if (event === "create") {
+        console.log("in middleware created " + ctx.path);
+      } else if (event === "modify") {
+        console.log("in middleware modify " + ctx.path);
+      } else if (event === "remove") {
+        console.log("in middleware remove " + ctx.path);
+      }
+    });
 
     await watcher.watch();
   }
