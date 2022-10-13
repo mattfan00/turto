63aa409841e15195f13fff463561a5e0fb24f04e
Matthew Fan <kungfumo@gmail.com>
2022-10-12 22:01:25 -0400

    added ignore paths to Watcher

--- cli/handlers/build.ts
+++ cli/handlers/build.ts
@@ -1,6 +1,6 @@
 import { turto } from "../../mod.ts";
 import { styles } from "../utils.ts";
-import { Watcher } from "../../deps.ts";
+import { path, Watcher } from "../../deps.ts";
 
 export interface BuildOptions {
   src?: string;
@@ -13,7 +13,9 @@ export const buildHandler = async (options: BuildOptions) => {
   site.build();
 
   if (options.watch) {
-    const watcher = new Watcher(site.options.src);
+    const watcher = new Watcher(site.options.src, {
+      ignore: `${path.join(site.getSrc(), "public/**")}`
+    });
     console.log(
       `Started watching for changes in ${styles.file(site.getSrc())}\n`,
     );
